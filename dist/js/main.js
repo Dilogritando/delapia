17f9784861aa7a46ea1959facf6f76e6b2eaede9
MarianaC <cortesruedam@gmail.com>
2021-08-03 10:45:55 -0500

    MVP Estilos 3 breakpoints

---
+++ dist/js/main.js
@@ -0,0 +1,14 @@
+//Show menu + close
+let menubtn = document.getElementById("icon_menu");
+let menuMobile = document.getElementById("nav");
+menubtn.addEventListener("click", mostrarMenu)
+function mostrarMenu(){
+    console.log("Hice click en menú")
+    menuMobile.classList.toggle("visible")
+}
+
+//Show prices + close
+
+//Form
+
+//Modal open + close
