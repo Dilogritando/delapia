e50cea03a4e4db914cab3faaaca938cdecb7ed1c
MarianaC <cortesruedam@gmail.com>
2021-08-03 18:49:55 -0500

    Ver precios y zona de cobertura

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,11 +3,28 @@ let menubtn = document.getElementById("icon_menu");
 let menuMobile = document.getElementById("nav");
 menubtn.addEventListener("click", mostrarMenu)
 function mostrarMenu(){
-    console.log("Hice click en menú")
     menuMobile.classList.toggle("visible")
 }
 
 //Show prices + close
+let allPrices = document.getElementsByClassName("precios");
+let i;
+for (i = 0; i < allPrices.length; i++) {
+    let parent = allPrices[i].closest("div");
+    let priceTable = parent.querySelector(".porciones_precios");
+    function showPrice() {
+        priceTable.classList.toggle("visible");
+    };
+    allPrices[i].addEventListener("click", showPrice);
+};
+
+//Valida + close
+let validaBtn = document.getElementById("formulario_valida-title")
+let mapaForm = document.getElementById("formulario_mapa")
+validaBtn.addEventListener("click", mostrarMapa)
+function mostrarMapa(){
+    mapaForm.classList.toggle("visible")
+}
 
 //Form
 
